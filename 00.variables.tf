24665e44246cf391d788d637fce00fc48df3a2c0
sujin <81570268+JoSujin-cando@users.noreply.github.com>
2026-01-29 14:32:17 +0900

    Remove default values for AWS access key
    Removed default values for AWS access keys to enhance security.

--- 00.variables.tf
+++ 00.variables.tf
@@ -1,12 +1,13 @@
 variable "AWS_ACCESS_KEY_ID" {
   description = "AWS access key"
   type        = string
-  default     = "AKIA463Z63VGSOEHQ75R"
+  default     = ""
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
   description = "AWS Secret Access Key"
   type        = string
   sensitive   = true
-  default     = "vja/KePyJBc578kghyA+PqXZMtWHClXA834W2l85"
+  default     = ""
 }
+
